2d3415d05f56de4bdc82e5a51a4c95b96618be8a
Yusuf Mostafa <ymostafa30@gmail.com>
2021-12-08 22:52:40 -0800

    Update test/release database

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -15,13 +15,12 @@ function LandingPage(props) {
             <div className='appAside' />
             <div className='appForm'>
                 <hr/>
-                <p>Things below the hr tag is just for development to make things easier for people to view the page they are editing.</p>
-                <button onClick={() => routeChange('/')}>LandingPage</button>
+                {/* <button onClick={() => routeChange('/')}>LandingPage</button> */}
                 <button onClick={() => routeChange('/signin')}>SignIn</button>
                 <button onClick={() => routeChange('/signup')}>Register</button>
-                <button onClick={() => routeChange('/home')}>UserLanding</button>
+                {/* <button onClick={() => routeChange('/home')}>UserLanding</button>
                 <button onClick={() => routeChange('/transactions')}>Register</button>
-                <button onClick={() => routeChange('/listing')}>Listing</button>
+                <button onClick={() => routeChange('/listing')}>Listing</button> */}
             </div>
         </div>
     );
